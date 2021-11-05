ff989b209e00da61f1f458c623437703ea2e2add
unknown <manikandan.it23@gmail.com>
2021-11-05 13:14:02 +0530

    OP-patientreg form design

--- src/app/patient-registration/patient-registration.component.ts
+++ src/app/patient-registration/patient-registration.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Patient } from './patient.model';
 @Component({
   selector: 'app-patient-registration',
   templateUrl: './patient-registration.component.html',
   styleUrls: ['./patient-registration.component.scss']
 })
 export class PatientRegistrationComponent implements OnInit {
-
+  patientRegObj: Patient = { name: '', dob: new Date(), address: '',sex:'', email: '', contact: '', fvDate:new Date(), communicationAddress:'', pincode:'',lastDoc:''};
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  registerPatient() {
+console.log(this.patientRegObj);
+  }
 }
---
+++ src/app/patient-registration/patient.model.ts
@@ -0,0 +1,12 @@
+export interface Patient {
+    name:string,
+    dob:Date,
+    address:string,
+    email:string,
+    contact:string,
+    fvDate:Date,
+    communicationAddress:string,
+    lastDoc:String,
+    pincode:string,
+    sex:string
+}
