3225ef3f5adaf89db9220c734501f4d12b09417b
ArunEj69 <arundes69@gmail.com>
2022-11-02 07:30:04 +0530

    latest update & issue changes.

--- src/app/supplier-payment/supplier-payment.service.ts
+++ src/app/supplier-payment/supplier-payment.service.ts
@@ -13,7 +13,22 @@ export class SupplierPaymentService {
     public getPoSuppPay(branch_id: any, supplierId: any): Observable<any> {
         let headers = new HttpHeaders();
         headers.append('Content-Type', 'application/json');
-        return this.http.get(environment.apiUrl + "popayschedulelists/KRC/" + branch_id +"/supplier_id=" +supplierId +"?payment_status=U",
+        return this.http.get(environment.apiUrl + "popayschedulelists/KRC/" + branch_id +"/" +supplierId +"?payment_status=U",
           { headers: headers })
     }
+
+    createPayment(param:any):Observable<any>{
+      let headers = new HttpHeaders();
+      headers.append('Content-Type', 'application/json');
+      return this.http.post(environment.apiUrl + 'createposupplierpayment',param,
+        { headers: headers })
+    }
+
+    public getSuppPayDetails(branch_id: any, supplierId: any, poNumber: any, suppInvNum: any): Observable<any> {
+      let headers = new HttpHeaders();
+      headers.append('Content-Type', 'application/json');
+      return this.http.get(environment.apiUrl + "posupplierpayments/KRC/" + branch_id +"/" +supplierId +"?po_number=" + poNumber + '&supp_inv_number=' + suppInvNum,
+        { headers: headers })
+  }
+    
 }
--- src/app/supplier-product/supplier-product.component.ts
+++ src/app/supplier-product/supplier-product.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { BillingService } from '../billing/billing.service';
+import { EodService } from '../eod/eod.service';
+import { PoService } from '../po/po.service';
 import { InfoDialogComponent } from '../utilities/info-dialog/info-dialog.component';
 import { SupplierProductService } from './supplier-product.service';
 
@@ -41,20 +43,27 @@ export class SupplierProductComponent implements OnInit {
     active_flag: ''
     // check: 0
   }
+  branchList: any;
+  eodDate: any;
   // buForm!: FormGroup;
 
   constructor(private supplierProdSer: SupplierProductService,
     private formBuilder: FormBuilder, private dialog: MatDialog,
-    private bs: BillingService,) { }
+    private bs: BillingService, private pos: PoService, private eodService: EodService) { }
 
   ngOnInit(): void {
     this.supplierProduct();
     this.getSupplierId();
     // this.getProductList();
     this.fetchBu();
-    // this.buForm = this.formBuilder.group({
-    //   bu_id: ['']
-    // })
+    this.getBranch();
+    this.loadBranchData();
+    this.getEodDetails();
+  }
+
+  loadBranchData() {
+    const id = localStorage.getItem('branch_id');
+    this.supplierProductForm.controls.branch_id.setValue(id);
   }
 
   supplierProduct() {
@@ -109,16 +118,26 @@ export class SupplierProductComponent implements OnInit {
 
   }
 
+  //Get EOD
+  getEodDetails() {
+    let orgId = localStorage.getItem('org_id')
+    let branch_id = localStorage.getItem('branch_id')
+    this.eodService.getEodDetailData(orgId, branch_id).subscribe(data => {
+      console.log("EOD data", data.results);
+      this.eodDate = data.results[0].eod_date;
+    })
+  }
+
   getProductList(id: any) {
     let bu_id = this.supplierProductForm.value.bu_id;
-    let branch_id = localStorage.getItem('branch_id')
-    this.supplierProdSer.getSupplierProdList(bu_id, branch_id).subscribe(data => {
+    let branch_id = localStorage.getItem('branch_id');
+    this.supplierProdSer.getSupplierProdList(bu_id, branch_id, this.eodDate).subscribe(data => {
       // this.tableData.forEach(element => {
       //   if(element.id == id){
           this.productList = data.results;
       //   }
       // });
-      // console.log("productList", this.productList);
+      console.log("productList", this.productList);
     })
   }
 
@@ -196,5 +215,13 @@ export class SupplierProductComponent implements OnInit {
 
   fetchProductsDynamic() {
     this.supplierProductForm.value.bu_id ? this.getProductList(this.supplierProductForm.value.bu_id) : '';
+  }
+
+  //getBranch
+  getBranch(){
+    this.pos.getBranchList().subscribe(data => {
+      console.log(data);
+      this.branchList = data.results;
+    })
   }
 }
