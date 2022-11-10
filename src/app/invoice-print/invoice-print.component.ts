9c5529ad98f10c21bcd3fe2dd4b931c5acb9260a
ArunEj69 <arundes69@gmail.com>
2022-11-10 22:33:36 +0530

    invoice-print changes.

--- src/app/invoice-print/invoice-print.component.ts
+++ src/app/invoice-print/invoice-print.component.ts
@@ -49,7 +49,14 @@ export class InvoicePrintComponent implements OnInit {
     this.orgId = localStorage.getItem('org_id');
     this.branchId = localStorage.getItem('branch_id');
     this.getReferenceList();
+    this.loadInvData();
     // this.fetchInvoiceStatus('A');
+  }
+
+  loadInvData(){
+    if(this.invoicePrintService.getInvData()){
+      this.print(this.invoicePrintService.getInvData());
+    }
   }
 
   getReferenceList() {
@@ -75,11 +82,6 @@ export class InvoicePrintComponent implements OnInit {
   }
 
   getInvoiceData(item: any) {
-    // let params = new HttpParams();
-    // params = params.append("org_id", this.orgId);
-    // params = params.append("branch_id", this.branchId);
-    // params = params.append("invoice_no", item.invoice_no,);
-    // params = params.append("patient_number", item.patient_id);
 
     this.invoicePrintService.getInvoiceList(item.invoice_no,item.patient_id).subscribe(
       (data) => {
@@ -127,18 +129,6 @@ export class InvoicePrintComponent implements OnInit {
           this.patientInvoiceDetail = true;
         }
       })
-      // this.invoicePrintService.fetchInvoiceSection(data.patient_id).subscribe(data => {
-      //   this.patientInvoiceDetail = true;
-      //   this.invoiceDetails = data.results;
-      //   // console.log("invoice",this.invoiceDetails)
-      // }, error => {
-      //   if (error.error.status === 404) {
-      //     this.dialog.open(InfoDialogComponent, {
-      //       width: '300px',
-      //       data: 'Invoice details not found!!!'
-      //     });
-      //   }
-      // })
     });
   }
 
