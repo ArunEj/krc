ad9e94faf3410af68fe31f6ade6fbb9f713f0f93
ArunEj69 <arundes69@gmail.com>
2022-11-20 13:24:56 +0530

    account master changes.

--- src/app/account-master/account-master.component.ts
+++ src/app/account-master/account-master.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { PaymentReceiptsService } from '../payment-receipts/payment-receipts.service';
 import { AccountMasterService } from './account-master.service';
 
 @Component({
@@ -9,11 +11,17 @@ import { AccountMasterService } from './account-master.service';
 })
 export class AccountMasterComponent implements OnInit {
   accForm!: FormGroup;
+  fetchData: any;
+  dataSource: any;
+  displayedColumns: string[] = ['account_code', 'account_desc', 'account_type', 'active_flag', 'edit'];
+  isShowEdit: boolean = false;
 
-  constructor(private formBuilder: FormBuilder, private amService: AccountMasterService) { }
+  constructor(private formBuilder: FormBuilder, private amService: AccountMasterService, private prs: PaymentReceiptsService) { }
 
   ngOnInit(): void {
     this.acc();
+    this.accForm.controls.status.setValue('Y');
+    this.changeAccount();
   }
 
   acc() {
@@ -33,11 +41,27 @@ export class AccountMasterComponent implements OnInit {
     let params = {
       org_id: localStorage.getItem('org_id'),
       user_id: localStorage.getItem('user_id'),
-      account_type: this.accForm.controls.accType.value,
+      account_type: this.accForm.controls.acc_type.value,
       account_desc: this.accForm.controls.acc_desc.value  
     }
+    console.log(params)
     this.amService.createAM(params).subscribe(data => {
       console.log(data);
     })
+  }
+
+  changeAccount() {
+    this.prs.fetchAcMasterByType(this.accForm.controls.acc_type.value).subscribe(data => {
+      this.fetchData = data.results;
+      this.dataSource = new MatTableDataSource(this.fetchData);
+    })
+  }
+
+  edit(element: any) {
+    this.isShowEdit = true;
+    console.log(element)
+    this.accForm.controls.acc_code.setValue(element.account_code);
+    this.accForm.controls.acc_desc.setValue(element.account_desc);
+    this.accForm.controls.status.setValue(element.active_flag);
   }
 }
--- src/app/doctor-master/doctor-master.component.ts
+++ src/app/doctor-master/doctor-master.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { AptBookingService } from '../apt-booking/apt-booking.service';
 import { DoctorMasterService } from './doctor-master.service';
 
 @Component({
@@ -9,26 +10,35 @@ import { DoctorMasterService } from './doctor-master.service';
 })
 export class DoctorMasterComponent implements OnInit {
   docForm!: FormGroup;
+  doctorList: any[] = [];
+  search: any;
 
-  constructor(private formBuilder: FormBuilder, private dmService: DoctorMasterService) { }
+  constructor(private formBuilder: FormBuilder, private dmService: DoctorMasterService, private aptService: AptBookingService) { }
 
   ngOnInit(): void {
     this.doc();
+    this.fetchDoctorsByBranchId();
   }
 
   doc() {
     this.docForm = this.formBuilder.group(
       {
-        doc_name: ['', []],
-        doc_id: ['', ],
-        doc_contact: ['', []],
-        doc_email: ['', []],
-        doc_assistence: ['', []],
-        doc_assist_contact: ['', []],
-        doc_assist_email: ['', []],
+        doctor_name: ['', []],
+        doctor_id: ['', ],
+        doctor_contact_no: ['', []],
+        doctor_email_id: ['', []],
+        doctor_assistant_name: ['', []],
+        doctor_assistant_contact_no: ['', []],
+        doctor_asst_emailid: ['', []],
         doc_attach: ['', []]
       }
     );
+  }
+
+  fetchDoctorsByBranchId() {
+    this.aptService.fetchDoctors().subscribe(data => {
+      this.doctorList = data.results;
+    })
   }
 
   createDoc() {
@@ -37,12 +47,12 @@ export class DoctorMasterComponent implements OnInit {
       org_id: localStorage.getItem('org_id'),
       branch_id: localStorage.getItem('branch_id'),
       user_id: localStorage.getItem('user_id'),
-      doctor_name: this.docForm.controls.doc_name.value,   
-      doctor_contact_no: this.docForm.controls.doc_contact.value,
-      doctor_email_id:this.docForm.controls.doc_email.value,
-      doctor_assistant_name: this.docForm.controls.doc_assistence.value,   
-      doctor_assistant_contact_no: this.docForm.controls.doc_assist_contact.value,
-      doctor_asst_emailid: this.docForm.controls.doc_assist_email.value,
+      doctor_name: this.docForm.controls.doctor_name.value,   
+      doctor_contact_no: this.docForm.controls.doctor_contact_no.value,
+      doctor_email_id:this.docForm.controls.doctor_email_id.value,
+      doctor_assistant_name: this.docForm.controls.doctor_assistant_name.value,   
+      doctor_assistant_contact_no: this.docForm.controls.doctor_assistant_contact_no.value,
+      doctor_asst_emailid: this.docForm.controls.doctor_asst_emailid.value,
       doctor_status:"Y"
     }
     this.dmService.createDM(params).subscribe(data => {
@@ -57,16 +67,39 @@ export class DoctorMasterComponent implements OnInit {
       org_id: localStorage.getItem('org_id'),
       branch_id: localStorage.getItem('branch_id'),
       user_id: localStorage.getItem('user_id'),
-      doctor_name: this.docForm.controls.doc_name.value,   
-      doctor_contact_no: this.docForm.controls.doc_contact.value,
-      doctor_email_id:this.docForm.controls.doc_email.value,
-      doctor_assistant_name: this.docForm.controls.doc_assistence.value,   
-      doctor_assistant_contact_no: this.docForm.controls.doc_assist_contact.value,
-      doctor_asst_emailid: this.docForm.controls.doc_assist_email.value,
+      doctor_name: this.docForm.controls.doctor_name.value,   
+      doctor_contact_no: this.docForm.controls.doctor_contact_no.value,
+      doctor_email_id:this.docForm.controls.doctor_email_id.value,
+      doctor_assistant_name: this.docForm.controls.doctor_assistant_name.value,   
+      doctor_assistant_contact_no: this.docForm.controls.doctor_assistant_contact_no.value,
+      doctor_asst_emailid: this.docForm.controls.doctor_asst_emailid.value,
       doctor_status:"Y"
     }
     this.dmService.createDM(params).subscribe(data => {
       console.log(data);
     })
+  }
+
+  setProductCost(data: any) {
+    let branchId = localStorage.getItem('branch_id');
+
+    this.doctorList.forEach((element: any) => {
+      if(data === element.doctor_name){
+        this.docForm.controls.doctor_id.setValue(element.doctor_id);
+        this.docForm.controls.doctor_contact_no.setValue(element.doctor_contact_no);
+        this.docForm.controls.doctor_email_id.setValue(element.doctor_email_id);
+        this.docForm.controls.doc_assistant_name.setValue(element.doctor_assistant_name);
+        this.docForm.controls.doctor_assistant_contact_no.setValue(element.doctor_assistant_contact_no);
+        this.docForm.controls.doctor_asst_emailid.setValue(element.doctor_asst_emailid);
+      }
+    });
+
+    // if(this.docForm.controls.doctor_id.value  !== null){
+    //   let supplierId = this.docForm.controls.supplier_id.value;
+    //   // this.dmService.fetchSupplier(branchId, supplierId).subscribe(data => {
+    //   //   this.fetchDocdata = data.results;
+    //   //   this.docForm();
+    //   // })
+    // }
   }
 }
