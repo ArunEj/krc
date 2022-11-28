7eff5e8b3cf89eefe434907351d1a80daac80d56
svastha2021 <manikandan.it23@gmail.com>
2022-11-28 20:33:06 +1030

    environment config changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-   //apiUrl:'http://45.79.126.173:4003/v1/',
- apiUrl: 'http://192.46.215.71:4003/v1/',
-  local:'http://localhost:4003/v1/'
+  apiUrl: 'https://node-prod.vibhavatech.com/v1/',
+  // apiUrl: 'http://192.46.215.71:4003/v1/',
+  local: 'http://localhost:4003/v1/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://192.46.215.71:4003/v1/',
+  apiUrl: 'https://node-prod.vibhavatech.com/v1/',
   // apiUrl:'http://45.79.126.173:4003/v1/',
-  local:'http://localhost:4003/v1/'
+  local: 'http://localhost:4003/v1/'
 };
 
 /*
